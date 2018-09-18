bdb16c8dd49ed1391a68cf82f6f177b09eaf0bb3
zubialevich <whitedemon9@gmail.com>
2018-09-18 23:53:51 +0200

    Updates

--- src.e40b5eef.js
+++ src.e40b5eef.js
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '50087' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '53800' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
