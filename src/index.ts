3c941c2e4b69d13cc296f7a150cb05c8a13e7904
takanotume24 <takanotume24@gmail.com>
2020-04-17 20:14:22 +0900

    worked

--- src/index.ts
+++ src/index.ts
@@ -1,78 +1,54 @@
-class TickInputHandler {
-    static epochTicks: number = 621355968000000000;
-    static ticksPerMillisecond: number = 10000;
-    static maxDateMilliseconds: number = 8640000000000000; // http://ecma-international.org/ecma-262/5.1/#sec-15.9.1.1
+class OriginalInputHandler {
 
     constructor() {
-        var inputElement = document.getElementById("ticks") as HTMLInputElement;
-        TickInputHandler.showResult(inputElement);
+        var original_element = document.getElementById("original") as HTMLInputElement;
+        OriginalInputHandler.showResult(original_element);
     }
 
     public doWork(event: KeyboardEvent) {
         const target = event.target as HTMLElement;
-        if (!TickInputHandler.isTickInput(target)) {
+
+        var original_element = target as HTMLInputElement;
+        OriginalInputHandler.showResult(original_element);
+    }
+
+    static showResult(original_element: HTMLInputElement) {
+
+        var converted_element = document.getElementById("converted");
+        if (!converted_element) {
             return;
         }
 
-        var inputElement = target as HTMLInputElement;
-        TickInputHandler.showResult(inputElement);
+        var source = original_element.value;
+        source = source.replace(/-\n/g, "")
+        source = source.replace(/\n/g, " ")
+        source = source.replace(/- /g, "")
+        source = source.replace(/Fig. /g, "Fig.")
+        source = source.replace(/No. /g, "No.")
+        source = source.replace(/Prof. /g, "Prof.")
+        source = source.replace(/Eq. /g, "Eq.")
+        source = source.replace(/et al. /g, "et al.")
+        var result_string = ""
+        const strings = source.split(". ").map(str => `${str}.`)
+        const WORD_LIMIT = 4500
+        var word_count = 0
+        strings.forEach(
+            string => {
+                result_string += `${string}\n`
+                word_count += string.length
+                if (word_count > WORD_LIMIT) {
+                    result_string += `\n==================\n\n`
+                    word_count = 0
+                }
+            }
+        )
+        console.log(strings)
+        converted_element.innerHTML = result_string
     }
 
-    static showResult(inputElement: HTMLInputElement) {
-        var value = TickInputHandler.getTickInputValueAsNumber(inputElement);
-
-        var dateTimeOutput = document.getElementById("datetime");
-        if (!dateTimeOutput) {
-            return;
-        }
-
-        var dateString = TickInputHandler.parseTicks(value);
-        if (!dateString) {
-            return;
-        }
-
-        var goodParts = /([0-9]+)/g;
-        var wrapParts = "<b>$1</b>";
-        dateString = dateString.replace(goodParts, wrapParts);
-
-        var firstTIndext = dateString.indexOf("T");
-        var datePart = dateString.substr(0, firstTIndext);
-        var timePart = dateString.substr(firstTIndext + 1);
-
-        dateTimeOutput.innerHTML =
-            datePart
-            + "<span class='pad'>T</span>"
-            + timePart;
-    }
-
-    static parseTicks(ticks: number): string {
-        if (isNaN(ticks)) {
-            return "____-__-__T__:__:__.____Z";
-        }
-
-        // convert the ticks into something javascript understands
-        var ticksSinceEpoch = ticks - TickInputHandler.epochTicks;
-        var millisecondsSinceEpoch = ticksSinceEpoch / TickInputHandler.ticksPerMillisecond;
-        if (millisecondsSinceEpoch > TickInputHandler.maxDateMilliseconds) {
-            //      +035210-09-17T07:18:31.111Z
-            return "9999-99-99T99:99:99:9999Z";
-        }
-        // output the result in something the human understands
-        var date = new Date(millisecondsSinceEpoch);
-        return date.toISOString();
-    }
-
-    static isTickInput(target: HTMLElement) {
-        return target.tagName == 'INPUT' && target.id == 'ticks';
-    }
-
-    static getTickInputValueAsNumber(inputElement: HTMLInputElement) {
-        var valueStr = inputElement.value;
-        return Number(valueStr);
-    }
 }
 
 window.onload = () => {
-    var handler = new TickInputHandler();
+    var handler = new OriginalInputHandler();
     document.addEventListener("keyup", handler.doWork);
 };
