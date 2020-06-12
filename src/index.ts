d10ae92b155fe6c32aafcb6cf5615c4cadbe2720
takanotume24 <takanotume24@gmail.com>
2020-06-13 02:05:58 +0900

    add support Neurology, fix regex, no lookbehind

--- src/index.ts
+++ src/index.ts
@@ -40,8 +40,8 @@ class OriginalInputHandler {
         source = source.replace(/et al\. /g, "et al.")
         source = source.replace(/Dr\. /g, "Dr.")
         source = source.replace(/e\.g\. /g, "e.g.")
-        source = source.replace(/(?<=\.)[\d+,]+[\d+](?= [A-Z])/g, "[$&]. ") //for "Neurology"
-        source = source.replace(/(?<=\.)[\d+]–[\d+](?= [A-Z])/g, "[$&]. ") //for "Neurology", "–" is not hyphen ...
+        source = source.replace(/\.[\d+,]+[\d+](?= [A-Z])/g, "[$&]. ") //for "Neurology"
+        source = source.replace(/\.[\d+]–[\d+](?= [A-Z])/g, "[$&]. ") //for "Neurology", "–" is not hyphen ...
 
         const strings = source.split(". ").map(str => `${str}.\n`)
         var results: string[][] = this.spilit_array(strings)
