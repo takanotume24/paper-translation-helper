2ac5238edc07dcd82edb9476686db34b21f842aa
MATSUNAGA Takuya <matsunaga.takuya.mt2@is.naist.jp>
2023-03-19 16:27:47 +0900

    update node packages

---
+++ update_packages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -eux
+
+# node.js - How to update each dependency in package.json to the latest version? - Stack Overflow
+# https://stackoverflow.com/questions/16073603/how-to-update-each-dependency-in-package-json-to-the-latest-version
+
+npx npm-check-updates -u
+npm install  
