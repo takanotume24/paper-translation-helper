fbe23167382fd650f0eaaed7d89a1f6a889ea870
MATSUNAGA Takuya <matsunaga.takuya.mt2@is.naist.jp>
2023-10-10 15:55:22 +0900

    update packages

--- update_packages.sh
+++ update_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux
 
-cd "$(dirname $0)"
+cd "$(dirname "$0")"
 
 # node.js - How to update each dependency in package.json to the latest version? - Stack Overflow
 # https://stackoverflow.com/questions/16073603/how-to-update-each-dependency-in-package-json-to-the-latest-version
