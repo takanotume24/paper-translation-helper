09650b13a78fa23e8fd1c6f6f0c35152beb47333
MATSUNAGA Takuya <matsunaga.takuya.mt2@is.naist.jp>
2024-02-15 10:57:59 +0900

    RegExpにはString.rawを渡すように変更

--- src/lib/format_and_split_text_Into_columns.ts
+++ src/lib/format_and_split_text_Into_columns.ts
@@ -16,32 +16,32 @@ export function formatAndSplitTextIntoColumns(
         [/-\n/g, ''], // Ensure hyphen followed by newline is completely removed
         [/\n/g, " "], // Replaces newlines with spaces
         [/- /g, ""], // Removes hyphens followed by a space
-        [RegExp(`Fig${dummy}`, "g"), "Fig. "], // Formats abbreviation for "Figure"
-        [RegExp(`Figs${dummy}`, "g"), "Figs. "], // Formats abbreviation for "Figures"
-        [RegExp(`No${dummy}`, "g"), "No. "], // Formats abbreviation for "Number"
-        [RegExp(`Prof${dummy}`, "g"), "Prof. "], // Formats abbreviation for "Professor"
-        [RegExp(`Eq${dummy}`, "g"), "Eq. "], // Formats abbreviation for "Equation"
-        [RegExp(`eq${dummy}`, "g"), "eq. "], // Formats abbreviation for "Equation"
-        [RegExp(`et al${dummy}`, "g"), "et al. "], // Formats "et al."
-        [RegExp(`Dr${dummy}`, "g"), "Dr. "], // Formats abbreviation for "Doctor"
-        [RegExp(`e\.g${dummy}`, "g"), "e.g. "], // Formats "e.g."
-        [RegExp(`i\.e${dummy}`, "g"), "i.e. "], // Formats "i.e."
-        [RegExp(`etc${dummy}`, "g"), "etc. "], // Formats "etc."
-        [RegExp(`Sec${dummy}`, "g"), "Sec. "], // Formats abbreviation for "Section"
-        [RegExp(`Sect${dummy}`, "g"), "Sect. "], // Formats abbreviation for "Section"
-        [RegExp(`Section${dummy}`, "g"), "Section. "], // Formats abbreviation for "Section"
-        [RegExp(`vs${dummy}`, "g"), "vs. "], // Formats abbreviation for "vs."
-        [RegExp(`2\.4 GHz`, "g"), "2.4GHz"], // Formats specific frequency value
-        [RegExp(`I${dummy}`, "g"), "I. "],
-        [RegExp(`II${dummy}`, "g"), "II. "],
-        [RegExp(`III${dummy}`, "g"), "III. "],
-        [RegExp(`IV${dummy}`, "g"), "IV. "],
-        [RegExp(`V${dummy}`, "g"), "V. "],
-        [RegExp(`VI${dummy}`, "g"), "VI. "],
-        [RegExp(`VII${dummy}`, "g"), "VII. "],
-        [RegExp(`VIII${dummy}`, "g"), "VIII. "],
-        [RegExp(`IX${dummy}`, "g"), "IX. "],
-        [RegExp(`X${dummy}`, "g"), "X. "],
+        [RegExp(String.raw`Fig${dummy}`, "g"), "Fig. "], // Formats abbreviation for "Figure"
+        [RegExp(String.raw`Figs${dummy}`, "g"), "Figs. "], // Formats abbreviation for "Figures"
+        [RegExp(String.raw`No${dummy}`, "g"), "No. "], // Formats abbreviation for "Number"
+        [RegExp(String.raw`Prof${dummy}`, "g"), "Prof. "], // Formats abbreviation for "Professor"
+        [RegExp(String.raw`Eq${dummy}`, "g"), "Eq. "], // Formats abbreviation for "Equation"
+        [RegExp(String.raw`eq${dummy}`, "g"), "eq. "], // Formats abbreviation for "Equation"
+        [RegExp(String.raw`et al${dummy}`, "g"), "et al. "], // Formats "et al."
+        [RegExp(String.raw`Dr${dummy}`, "g"), "Dr. "], // Formats abbreviation for "Doctor"
+        [RegExp(String.raw`e\.g${dummy}`, "g"), "e.g. "], // Formats "e.g."
+        [RegExp(String.raw`i\.e${dummy}`, "g"), "i.e. "], // Formats "i.e."
+        [RegExp(String.raw`etc${dummy}`, "g"), "etc. "], // Formats "etc."
+        [RegExp(String.raw`Sec${dummy}`, "g"), "Sec. "], // Formats abbreviation for "Section"
+        [RegExp(String.raw`Sect${dummy}`, "g"), "Sect. "], // Formats abbreviation for "Section"
+        [RegExp(String.raw`Section${dummy}`, "g"), "Section. "], // Formats abbreviation for "Section"
+        [RegExp(String.raw`vs${dummy}`, "g"), "vs. "], // Formats abbreviation for "vs."
+        [RegExp(String.raw`2\.4 GHz`, "g"), "2.4GHz"], // Formats specific frequency value
+        [RegExp(String.raw`I${dummy}`, "g"), "I. "],
+        [RegExp(String.raw`II${dummy}`, "g"), "II. "],
+        [RegExp(String.raw`III${dummy}`, "g"), "III. "],
+        [RegExp(String.raw`IV${dummy}`, "g"), "IV. "],
+        [RegExp(String.raw`V${dummy}`, "g"), "V. "],
+        [RegExp(String.raw`VI${dummy}`, "g"), "VI. "],
+        [RegExp(String.raw`VII${dummy}`, "g"), "VII. "],
+        [RegExp(String.raw`VIII${dummy}`, "g"), "VIII. "],
+        [RegExp(String.raw`IX${dummy}`, "g"), "IX. "],
+        [RegExp(String.raw`X${dummy}`, "g"), "X. "],
         [/\.\d+,\d+(?= [A-Z])/g, match => "[" + match + "]. "], // Formats numbers with commas
         [/\.\d+-\d+(?= [A-Z])/g, match => "[" + match + "]. "], // Formats number ranges
     ];
--- src/lib/test/format_and_split_text_into_columns.test.ts
+++ src/lib/test/format_and_split_text_into_columns.test.ts
@@ -63,11 +63,22 @@ describe('formatAndSplitTextIntoColumns', () => {
     });
 
     it('should process and split text correctly [size. ]', () => {
-        const testString = " When shopping for clothes, it's essential to consider your body size. The small backpack comes in various colors and sizes.";
+        const testString = "When shopping for clothes, it's essential to consider your body size. The small backpack comes in various colors and sizes.";
         const charLimit = 10;
         const expectedResult = [
             ["When shopping for clothes, it's essential to consider your body size."],
             ['The small backpack comes in various colors and sizes.']
+        ];
+        const result = formatAndSplitTextIntoColumns(testString, charLimit);
+        expect(result).toEqual(expectedResult);
+    });
+
+    it('should process and split text correctly [time. ]', () => {
+        const testString = "All good things must come to an end, time. After this moment passes, we begin anew with lessons learned and memories cherished.";
+        const charLimit = 10;
+        const expectedResult = [
+            ["All good things must come to an end, time."],
+            ['After this moment passes, we begin anew with lessons learned and memories cherished.']
         ];
         const result = formatAndSplitTextIntoColumns(testString, charLimit);
         expect(result).toEqual(expectedResult);
