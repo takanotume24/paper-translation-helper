7001e413d302cf7dd38754b0e4b503fc488e0a2a
MATSUNAGA Takuya <matsunaga.takuya.mt2@is.naist.jp>
2024-12-06 12:34:45 +0900

    コンポーネントのリファクタリングと新規コンポーネントの追加
    - App.vue内のヘッダー、入力、出力、文字数制限の部分をそれぞれのコンポーネントに分割
    - 新しいコンポーネントを以下の通り追加:
      - Header.vue
      - TextInput.vue
      - TextOutput.vue
      - CharLimitInput.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a href="#" class="navbar-brand">📄📚 Paper Translation Helper 📚📄</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav mr-auto"></ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link"
+                                href="https://github.com/takanotume24/paper-translation-helper/wiki/%E4%BD%BF%E3%81%84%E6%96%B9">使い方</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link"
+                                href="https://github.com/takanotume24/paper-translation-helper/issues">ご意見･ご要望</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link"
+                                href="https://github.com/takanotume24/paper-translation-helper/">Repository</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://github.com/takanotume24/">@takanotume24</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'Header'
+};
+</script>
